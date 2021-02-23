3b17c1612f800b4199f9ae51543a621dcfbb197b
Ali Adruich <adruich.ali@gmail.com>
2021-02-23 19:37:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+       
+        <div className="grid-container">
+        <header> <a href="/">React Store</a></header>
+        <main>Products List</main>
+        <footer>All Right Resived</footer>
+        </div> 
+
+    )
 }
 
 export default App;

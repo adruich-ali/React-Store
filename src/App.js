21fd8a3c1c887110d1954ac0a555f1c8ec1ce2bd
Ali Adruich <adruich.ali@gmail.com>
2021-02-23 22:03:02 +0100

    product-list-comp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React  from 'react';
+import data from "./data.json";
+import { Component } from "react";
+import Products from './componentes/products';
 
-function App() {
-    return (
-       
-        <div className="grid-container">
-        <header> <a href="/">React Store</a></header>
-        <main>Products List</main>
-        <footer>All Right Resived</footer>
-        </div> 
-
-    )
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state ={
+            products:data.products,
+            size:"",
+            sort:"",
+        }
+    }
+    render(){
+        return (
+            <div className="grid-container">
+            <header> <a href="/">React Store</a></header>
+            <main>
+                <div className="content">
+                    <div className="main">
+Products              <Products products={this.state.products} />
+                    </div>
+                    <div className="sidebar">
+Cart Items
+                    </div>
+                </div>
+            </main>
+            <footer>All Right Resived</footer>
+            </div> 
+        );
+    }
 }
 
 export default App;
